4ec774eaca4479daac44f36458c46294103d1b0a
Ken Goad <goadke@g.cofc.edu>
2019-12-01 18:40:56 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
+  attr_accessor :name
+  attr_reader :yrs_experience
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
   
 end
